b7fafa673b67ff91298113703f261a8f79580e83
Pablo Díaz <pablo5_diaz@hotmail.com>
2017-08-30 10:13:04 -0600

    docs

--- server/v1/application/carrera.controller.ts
+++ server/v1/application/carrera.controller.ts
@@ -2,22 +2,36 @@ import { Carrera } from '../domain/carrera.model';
 import _db  from './persistence/db.repository';
 import { CarreraService } from './services/carrera.service';
 
+/**
+ * Get all entities
+ * @return Carrera[]
+ */
 const getAll = async (req, res ) => {
   try {
 
     const c_service = new CarreraService(_db);
     const carreras = await  c_service.obtenerCarreras();
+
     return res.json(carreras);
   } catch (err) {
     return res.json(err);
   }
 };
 
+/**
+ * Create new entity
+ * @param {string}  req.body.nombre  [Name of career]
+ * @param {string}  req.body.description [Description of career]
+ * @param {integer} req.body.estudiantes [quantity of students]
+ * @return {string[]} List of entities
+ */
 const createNewCarrera = async (req, res) => {
   try {
+
     const c_service = new CarreraService(_db);
     const carrera = new Carrera(req.body.nombre, req.body.descripcion, req.body.estudiantes);
     const saved_carrera = c_service.save(carrera);
+
     return res.json(saved_carrera);
   } catch (e){
     console.log(e);
