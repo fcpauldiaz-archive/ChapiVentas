7ff4cb5e6bbc3d57412d40470c16dfd000437147
Hsing-Li <chang@hsing.li>
2017-10-14 13:55:51 -0600

    statsd on  - promocion  - user

--- server/v1/application/promocion.controller.ts
+++ server/v1/application/promocion.controller.ts
@@ -5,30 +5,57 @@ import _db  from './persistence/db.repository';
 import { PromocionService } from './services/promocion.service';
 const amqp = require('amqplib');
 
+import { StatsD } from 'node-statsd/lib/statsd';
+
+var client = new StatsD({
+  host: 'ec2-52-90-161-71.compute-1.amazonaws.com'
+})
+
 const getMesPromociones = async (req, res) => {
+  let startingDate = new Date();
   try {
     const p_service = new PromocionService(_db);
     const promociones = await p_service.obtenerPromociones();
+
+    client.increment('promotions_get_by_month_success');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('promotions_get_by_month_response_time', timeDiff);
+
     return res.json(promociones);
   } catch(err) {
+
+    client.increment('promotions_get_by_month_error');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('promotions_get_by_month_response_time', timeDiff);
+
     return res.json(err);
   }
 }
 
 const sendEvent = async(topic, data) => {
- try {
+  let startingDate = new Date();
+  try {
     const conn = await amqp.connect('amqp://13.58.81.154');
     const ch = await conn.createChannel();
     ch.assertQueue(topic, { durable: true });
     ch.sendToQueue(topic, new Buffer.from(JSON.stringify(data)));
+
+    client.increment('promotions_send_event_success');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('promotions_send_event_response_time', timeDiff);
+
     console.log('promotion sent');
- } catch (e) {
-   console.log(e);
- }
+  } catch (e) {
+    console.log(e);
+    client.increment('promotions_send_event_error');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('promotions_send_event_response_time', timeDiff);
+  }
 
 }
 
 const createNewPromocion = async (req, res) => {
+  let startingDate = new Date();
   try {
     const p_service = new PromocionService(_db);
     const tipoPromocion = new TipoPromocion(req.body.tipo);
@@ -37,34 +64,56 @@ const createNewPromocion = async (req, res) => {
       req.body.fechaInicioPromo, req.body.fechaFinalPromo, req.body.color);
     const saved_promocion = await p_service.save(promocion);
     sendEvent('new_promotion', promocion);
-    return res.json(promocion);
 
+    client.increment('promotions_create_success');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('promotions_create_response_time', timeDiff);
+
+    return res.json(promocion);
   } catch (e) {
-    console.log(e);
+    client.increment('promotions_create_error');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('promotions_create_response_time', timeDiff);
     return res.json(e);
   }
 }
 
 const getPromocionFecha = async (req, res) => {
+  let startingDate = new Date();
   try {
     const p_service = new PromocionService(_db);
     const fechaInicio = new Date(req.query.fechaInicio);
     const fechaFinal = new Date(req.query.fechaFinal);
     const query = await p_service.obtenerPromocionesPorFecha(fechaInicio, fechaFinal);
+
+    client.increment('promotions_get_by_date_success');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('promotions_get_by_date_response_time', timeDiff);
+
     return res.json(query);
   } catch (e) {
-    console.log(e)
+    client.increment('promotions_get_by_date_error');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('promotions_get_by_date_response_time', timeDiff);
     return res.json(e);
   }
 }
 
 const deletePromocion = async (req, res) => {
+  let startingDate = new Date();
   try {
     const p_service = new PromocionService(_db);
     const response = await p_service.eliminarPromocion(req.body.id);
+
+    client.increment('promotions_delete_success');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('promotions_delete_response_time', timeDiff);
+
     return res.json(response);
   } catch (e) {
-    console.log(e);
+    client.increment('promotions_delete_error');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('promotions_delete_response_time', timeDiff);
     return res.json(e);
   }
 
--- server/v1/application/user.controller.ts
+++ server/v1/application/user.controller.ts
@@ -2,17 +2,33 @@ import { User } from '../domain/user.model';
 import { UserService } from './services/user.service';
 import _db  from './persistence/db.repository';
 
+import { StatsD } from 'node-statsd/lib/statsd';
+
+var client = new StatsD({
+  host: 'ec2-52-90-161-71.compute-1.amazonaws.com'
+})
 
 /**
  * Get user
  * @returns {User}
  */
 const get = async (req, res) => {
+  let startingDate = new Date();
   try {
     const u_service = new UserService(_db);
     const user = await u_service.buscarUsuario(req.params.username);
+
+    client.increment('users_get_success');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('users_get_response_time', timeDiff);
+
     return res.json(user);
   } catch (e) {
+
+    client.increment('users_get_error');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('users_get_response_time', timeDiff);
+
     return res.json(e);
   }
 };
@@ -22,11 +38,20 @@ const get = async (req, res) => {
  * @returns {User}
  */
 const list = async (req, res) => {
+  let startingDate = new Date();
   try {
     const u_service = new UserService(_db);
     const user = await u_service.obtenerUsuarios();
+
+    client.increment('users_list_success');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('users_list_response_time', timeDiff);
+
     return res.json(user);
   } catch (e) {
+    client.increment('users_list_error');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('users_list_response_time', timeDiff);
     return res.json(e);
   }
 };
@@ -39,32 +64,65 @@ const list = async (req, res) => {
  * @returns {User}
  */
 const create = async (req, res) => {
+  let startingDate = new Date();
   try {
     const user = new User(req.body.username, req.body.password);
     const u_service = new UserService(_db);
     const usr = await u_service.save(user);
+
+    client.increment('users_create_success');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('users_create_response_time', timeDiff);
+
     return res.json(usr);
   } catch (e) {
+
+    client.increment('users_create_error');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('users_create_response_time', timeDiff);
+
     return res.json(e);
   }
 };
 
 const authenticate = async (req, res) => {
+  let startingDate = new Date();
   try {
     const u_service = new UserService(_db);
     const usr = await u_service.auth(req.body.username, req.body.passowrd);
+
+    client.increment('users_authenticate_success');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('users_authenticate_response_time', timeDiff);
+
     return res.json(usr);
   } catch (e) {
+
+    client.increment('users_authenticate_error');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('users_authenticate_response_time', timeDiff);
+
     return res.json(e);
   }
 };
 
 const deleteUser = async (req, res) => {
+  let startingDate = new Date();
   try {
     const u_service = new UserService(_db);
     const usr = await u_service.eliminarUsuario(req.params.userId);
+
+    client.increment('users_delete_success');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('users_delete_response_time', timeDiff);
+
     return res.json(usr);
   } catch (e) {
+
+    client.increment('users_delete_error');
+    let timeDiff = (new Date()).valueOf() - (startingDate).valueOf()
+    client.timing('users_delete_response_time', timeDiff);
+
     return res.json(e);
   }
 };
